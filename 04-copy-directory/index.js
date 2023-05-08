5c8e7c2cd83fdda5f8a34170dbe4eb0c0ca5776e
AL5t <oalsim5@gmail.com>
2023-05-08 10:43:34 +0300

    feat: add solution for fourth task(04-copy-directory)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+  if (error) console.log(error);
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+    if (error) console.log(error);
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (error, files) => {
+      if (error) console.log(error);
+      for (const file of files) {
+        if (file.isFile()) {
+          fs.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`), (error) => {
+            if (error) console.log(error);
+          });
+        }
+      }
+    });
+  });
+});
