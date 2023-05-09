393c822eaf103c2f8c323c3152f4221b5a8e4cd3
AL5t <oalsim5@gmail.com>
2023-05-10 01:26:27 +0300

    feat: add a condition to check if the folder files-copy exists (04-copy-directory)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
-  if (error) console.log(error);
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
-    if (error) console.log(error);
-    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (error, files) => {
+fs.access(path.join(__dirname, 'files-copy'), error => {
+  if (error) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
       if (error) console.log(error);
-      for (const file of files) {
-        if (file.isFile()) {
-          fs.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`), (error) => {
-            if (error) console.log(error);
-          });
+      fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (error, files) => {
+        if (error) console.log(error);
+        for (const file of files) {
+          if (file.isFile()) {
+            fs.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`), (error) => {
+              if (error) console.log(error);
+            });
+          }
         }
-      }
+      });
     });
-  });
+  } else {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+      if (error) console.log(error);
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+        if (error) console.log(error);
+        fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (error, files) => {
+          if (error) console.log(error);
+          for (const file of files) {
+            if (file.isFile()) {
+              fs.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`), (error) => {
+                if (error) console.log(error);
+              });
+            }
+          }
+        });
+      });
+    });
+  }
 });
